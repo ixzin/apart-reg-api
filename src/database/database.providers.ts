c428fdaaba4e057c235eea5ebc9fe0074a6ba72d
obalandin <obalandin@deloitte.com>
2020-10-21 17:42:53 +0300

    db auth

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -5,7 +5,9 @@ export const databaseProviders = [
     provide: 'DATABASE_CONNECTION',
     useFactory: (): Promise<typeof mongoose> =>
       mongoose.connect('mongodb://localhost/apartdb',{
-        useCreateIndex: true
+        useCreateIndex: true,
+        user:"admin",
+        pass:"7k1f5d4t"
       }),
   },
 ];
